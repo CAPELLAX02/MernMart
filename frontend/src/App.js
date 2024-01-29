782448e3dac75f70d25c8a5667b238be84de9736
CAPELLAX02 <ahmetatar002@gmail.com>
2024-01-29 17:25:25 +0300

    frontend layout completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
-  return <h1>HELLO WORLD!!</h1>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
