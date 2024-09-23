e5a711e508d12d11c4cf1c603b1f47ab1d694395
CAPELLAX02 <ahmtatar@gmail.com>
2024-09-23 18:45:06 +0300

    JSDoc comment added in user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,13 +7,15 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import generateToken from '../utils/generateToken.js';
 
-// __dirname ve __filename'i oluşturmak için gerekli
+// Get the __filename and __dirname for EJS template rendering
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// @desc     Auth user & get token
-// @route    POST /api/users/login
-// @access   Public
+/**
+ * @desc    Authenticate user and get token
+ * @route   POST /api/users/login
+ * @access  Public
+ */
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
@@ -34,11 +36,13 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-// JWT oluşturma fonksiyonu (kayıt için)
+/**
+ * Generates a JWT token for account activation and a six-digit activation code.
+ * @param {Object} user - The user object containing registration details.
+ * @returns {{ token: string, activationCode: string }} - The JWT token and activation code.
+ */
 const createActivationToken = (user) => {
-  const activationCode = Math.floor(100000 + Math.random() * 900000).toString(); // 6 haneli kod
-
-  // console.log('Generated Activation Code:', activationCode); // Loglama ekleyin
+  const activationCode = Math.floor(100000 + Math.random() * 900000).toString();
 
   const token = jwt.sign(
     {
@@ -54,9 +58,11 @@ const createActivationToken = (user) => {
   return { token, activationCode };
 };
 
-// @desc     Kullanıcı Kaydı
-// @route    POST /api/users
-// @access   Public
+/**
+ * @desc    Register new user and send activation email
+ * @route   POST /api/users
+ * @access  Public
+ */
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -66,11 +72,9 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error('Bu e-posta adresi zaten kullanımda.');
   }
 
-  // Kullanıcı bilgileri ile bir activation token oluşturuyoruz
   const user = { name, email, password };
   const { token, activationCode } = createActivationToken(user);
 
-  // E-posta içeriğini oluşturuyoruz
   const data = { user: { name: user.name }, activationCode };
 
   const htmlContent = await ejs.renderFile(
@@ -78,7 +82,6 @@ const registerUser = asyncHandler(async (req, res) => {
     data
   );
 
-  // E-posta gönderiyoruz
   try {
     await sendMail({
       email: user.email,
@@ -87,24 +90,23 @@ const registerUser = asyncHandler(async (req, res) => {
       data,
     });
 
-    // Response ile activation_token'ı geri döndürüyoruz
     res.status(201).json({
       success: true,
       message: `Please check your email: ${user.email} to activate your account!`,
       activationToken: token, // activation_token'ı burada döndürüyoruz
     });
   } catch (error) {
-    // console.error('E-posta gönderilemedi:', error);
-    // console.log(error.stack);
     res.status(400).json({
       message: 'E-posta gönderilemedi, lütfen tekrar deneyin.',
     });
   }
 });
 
-// @desc     Kullanıcıyı Doğrulama
-// @router   POST /api/users/verify-email
-// @access   Public
+/**
+ * @desc    Verify user email after registration
+ * @route   POST /api/users/verify-email
+ * @access  Public
+ */
 const verifyUser = asyncHandler(async (req, res) => {
   try {
     const { activation_token, activation_code } = req.body;
@@ -135,14 +137,15 @@ const verifyUser = asyncHandler(async (req, res) => {
       userId: user._id,
     });
   } catch (error) {
-    // console.log('verifyUser catch block: ', error);
     res.status(400).json({ message: error.message });
   }
 });
 
-// @desc     Log out user & clear cookie
-// @route    POST /api/users/logout
-// @access   Private
+/**
+ * @desc    Logout user and clear cookie
+ * @route   POST /api/users/logout
+ * @access  Private
+ */
 const logoutUser = asyncHandler(async (req, res) => {
   res.cookie('jwt', '', {
     httpOnly: true,
@@ -151,9 +154,11 @@ const logoutUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Logged out successfully.' });
 });
 
-// @desc     Get user
-// @route    GET /api/users/profile
-// @access   Private
+/**
+ * @desc    Get user profile
+ * @route   GET /api/users/profile
+ * @access  Private
+ */
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
@@ -170,9 +175,11 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc     Update user
-// @route    PUT /api/users/profile
-// @access   Private
+/**
+ * @desc    Update user profile
+ * @route   PUT /api/users/profile
+ * @access  Private
+ */
 const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
@@ -180,7 +187,6 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
 
-    // Password is seperated from the others above because it's hashed.
     if (req.body.password) {
       user.password = req.body.password;
     }
@@ -194,14 +200,16 @@ const updateUserProfile = asyncHandler(async (req, res) => {
       isAdmin: updatedUser.isAdmin,
     });
   } else {
-    res.status(404); // not found (404)
+    res.status(404);
     throw new Error('User not found');
   }
 });
 
-// @desc     Get users
-// @route    GET /api/users
-// @access   Private/Admin
+/**
+ * @desc     Get all users
+ * @route    GET /api/users
+ * @access   Private/Admin
+ */
 const getUsers = asyncHandler(async (req, res) => {
   const users = await User.find({});
   if (users) {
@@ -212,9 +220,11 @@ const getUsers = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc     Get user by ID
-// @route    GET /api/users/:id
-// @access   Private/Admin
+/**
+ * @desc     Get user by ID
+ * @route    GET /api/users/:id
+ * @access   Private/Admin
+ */
 const getUserByID = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id).select('-password');
   if (user) {
@@ -225,9 +235,11 @@ const getUserByID = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc     Delete user
-// @route    DELETE /api/users/:id
-// @access   Private/Admin
+/**
+ * @desc     Delete user
+ * @route    DELETE /api/users/:id
+ * @access   Private/Admin
+ */
 const deleteUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id);
   if (user) {
@@ -243,9 +255,11 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc     Update user
-// @route    PUT /api/users/:id
-// @access   Private/Admin
+/**
+ * @desc     Update user
+ * @route    PUT /api/users/:id
+ * @access   Private/Admin
+ */
 const updateUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id);
   if (user) {
@@ -267,9 +281,11 @@ const updateUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc     Send password reset email
-// @route    POST /api/users/forgot-password
-// @access   Public
+/**
+ * @desc    Send forgot password email with reset code
+ * @route   POST /api/users/forgot-password
+ * @access  Public
+ */
 const sendForgotPasswordEmail = asyncHandler(async (req, res) => {
   const { email } = req.body;
   const user = await User.findOne({ email: email });
@@ -290,13 +306,13 @@ const sendForgotPasswordEmail = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc    Reset password
-// @route   POST /api/users/reset-password
-// @access  Private
+/**
+ * @desc    Reset password
+ * @route   POST /api/users/reset-password
+ * @access  Private
+ */
 const resetPassword = asyncHandler(async (req, res) => {
   const { email, resetPasswordCode, newPassword } = req.body;
-
-  // console.log('Received data:', { email, resetPasswordCode, newPassword });
 
   if (!email || !resetPasswordCode || !newPassword) {
     res.status(400);
@@ -308,8 +324,6 @@ const resetPassword = asyncHandler(async (req, res) => {
     resetPasswordCode: resetPasswordCode,
     resetPasswordCodeExpires: { $gt: Date.now() },
   });
-
-  // console.log('Found user:', user); // Kullanıcıyı loglama
 
   if (!user) {
     res.status(404);
