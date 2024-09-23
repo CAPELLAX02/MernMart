10a9e7fa728c5906eea6c7a6af4b6992664469dc
CAPELLAX02 <ahmtatar@gmail.com>
2024-09-23 17:41:54 +0300

    typescript project dependencies installed

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,14 +1,10 @@
-import mongoose, { connect } from 'mongoose';
-import dotenv from 'dotenv';
-import colors from 'colors';
+require('dotenv').config();
 import users from './data/users.js';
 import products from './data/products.js';
 import User from './models/userModel.js';
 import Product from './models/productModel.js';
 import Order from './models/orderModel.js';
 import connectDB from './config/db.js';
-
-dotenv.config();
 
 connectDB();
 
