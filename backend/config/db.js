c21a2b5257294e57b4efa61402ec058ffddda5a5
CAPELLAX02 <ahmtatar@gmail.com>
2024-09-24 10:21:18 +0300

    JSDoc added in database connection code

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,12 @@
 import mongoose from 'mongoose';
 
+/**
+ * Connect to MongoDB database
+ * @async
+ * @function connectDB
+ * @returns {Promise<void>}
+ * @throws {Error} If connection fails
+ */
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
