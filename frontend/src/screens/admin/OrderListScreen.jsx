34f2824d833328b2a2008bd09cb511aa3397d7ac
CAPELLAX02 <ahmtatar@gmail.com>
2024-09-28 18:30:22 +0300

    update readme.md

--- frontend/src/screens/admin/OrderListScreen.jsx
+++ frontend/src/screens/admin/OrderListScreen.jsx
@@ -11,23 +11,23 @@ const OrderListScreen = () => {
 
   return (
     <>
-      <h1 className='mt-3'>Orders</h1>
+      <h1 className="mt-3">Orders</h1>
       {isLoading ? (
         <Loader />
       ) : error ? (
-        <Message variant='danger'>
+        <Message variant="danger">
           Something went wrong while fetching the orders' data. [
           {error?.data?.message || error.error}]
         </Message>
       ) : (
-        <Table striped hover responsive className='mt-2'>
+        <Table striped hover responsive className="mt-2">
           <thead>
             <tr>
               <th>ORDER ID</th>
               <th>CUSTOMER</th>
               <th>DATE</th>
-              <th>COST</th>
-              <th>PAYMEMT</th>
+              <th>PRICE</th>
+              <th>PAYMENT</th>
               <th>DELIVERY</th>
               <th></th>
             </tr>
@@ -57,8 +57,8 @@ const OrderListScreen = () => {
                   <Button
                     as={Link}
                     to={`/order/${order._id}`}
-                    variant='info'
-                    className='btn-sm text-white'
+                    variant="info"
+                    className="btn-sm text-white"
                   >
                     Details
                   </Button>
